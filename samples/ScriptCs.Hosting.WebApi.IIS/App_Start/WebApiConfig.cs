5903a3a34a2101cce8d70ae6f8ff93fc76220f82
Mads Tjørnelund Toustrup <mtt@d60.dk>
2014-11-20 12:31:36 +0100

    Have a running revision.

--- samples/ScriptCs.Hosting.WebApi.IIS/App_Start/WebApiConfig.cs
+++ samples/ScriptCs.Hosting.WebApi.IIS/App_Start/WebApiConfig.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using Rebus;
+using ScriptCs.Rebus;
 
 namespace ScriptCs.Hosting.WebApi.IIS
 {
@@ -18,6 +21,18 @@ namespace ScriptCs.Hosting.WebApi.IIS
             var builder = new WebApiConfigurationBuilder(config, HttpContext.Current.Server.MapPath("bin"));
             builder.Build();
 
+            var bus = new MsmqBus("controllerBus");
+            bus.Container.Register(() => new ScriptedControllerHandler());
+            bus.Start();
+        }
+    }
+
+    public class ScriptedControllerHandler : IHandleMessages<Script>
+    {
+        public void Handle(Script message)
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"bin\Scripts\ScriptController.csx");
+            File.WriteAllText(path, message.ScriptContent);
         }
     }
 }
